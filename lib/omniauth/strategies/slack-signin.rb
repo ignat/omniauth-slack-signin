04354ac7f41b4f8a55a353d476e9e17dc0a2d114
Ignat Skoryh <ignat.skoryh@gmail.com>
2016-08-23 09:50:50 +0300

    Initial import

---
+++ lib/omniauth/strategies/slack-signin.rb
@@ -0,0 +1,44 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class SlackSignIn < OmniAuth::Strategies::OAuth2
+
+      option :name, "slack-signin"
+      option :authorize_options, [:scope]
+      option :client_options, {
+        site: "https://slack.com",
+        token_url: "/api/oauth.access"
+      }
+      option :auth_token_params, {
+        mode: :query,
+        param_name: 'token'
+      }
+
+      uid do
+        "#{identity['team']['id']}-#{identity['user']['id']}"
+      end
+
+      info do
+        {
+          user_name: identity['user']['name'],
+          user_id: identity['user']['id'],
+          team_id: identity['team']['id']
+        }
+      end
+
+      extra do
+        {
+          raw_info: identity
+        }
+      end
+
+      def identity
+        @identity ||= access_token.get('/api/users.identity').parsed
+      end
+
+    end
+  end
+end
+
+OmniAuth.config.add_camelization "slack-signin", "SlackSignIn"
---
+++ omniauth-slack-signin.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |spec|
+  spec.name = "omniauth-slack-signin"
+  spec.version = "0.1"
+  spec.authors = ["ignat"]
+  spec.email = ["ignat@yahoo.com"]
+  spec.description = "OmniAuth strategy for Slack Sign In"
+  spec.summary = "OmniAuth strategy for Slack Sign In"
+  spec.homepage = "https://github.com/ignat/omniauth-slack-signin"
+  spec.license = "MIT"
+  spec.require_paths = ["lib"]
+  spec.add_runtime_dependency 'omniauth-oauth2'
+end
